0ce2cd417e8c5d156a6dd8ccb0c8ec5a83a59e75
Iris Shi <0.0@owo.li>
2025-12-01 11:06:40 +0800

    refactor: simplify shell input logic, fix and add visual bugs

--- frontend/src/components/shell/input.rs
+++ frontend/src/components/shell/input.rs
@@ -1,117 +1,56 @@
-use crate::utils::sleep_ms;
-use wasm_bindgen_futures::spawn_local;
-use web_sys::{FocusEvent, HtmlInputElement};
+use web_sys::HtmlInputElement;
 use yew::prelude::*;
 
 #[derive(Properties, PartialEq)]
 pub struct ShellInputProps {
-    pub value: String,
+    pub value: AttrValue,
     pub on_change: Callback<String>,
     #[prop_or_default]
-    pub placeholder: String,
+    pub placeholder: AttrValue,
     #[prop_or_default]
     pub disabled: bool,
 }
 
-// FIXME: Text selection is completely broken
 #[function_component(ShellInput)]
 pub fn shell_input(props: &ShellInputProps) -> Html {
-    let selection_start = use_state(|| 0);
-    let selection_end = use_state(|| 0);
-    let is_focused = use_state(|| false);
     let input_ref = use_node_ref();
+    let is_focused = use_state(|| false);
+    let cursor_pos = use_state(|| 0);
 
-    {
-        let len = props.value.chars().count();
-        if *selection_start > len {
-            selection_start.set(len);
-        }
-        if *selection_end > len {
-            selection_end.set(len);
-        }
-    }
-
-    let update_selection = {
-        let selection_start = selection_start.clone();
-        let selection_end = selection_end.clone();
-        move |input: HtmlInputElement| {
-            if let Ok(Some(start)) = input.selection_start() {
-                selection_start.set(start as usize);
-            }
-            if let Ok(Some(end)) = input.selection_end() {
-                selection_end.set(end as usize);
-            }
-        }
+    let update_cursor = {
+        let cursor_pos = cursor_pos.clone();
+        Callback::from(move |input: HtmlInputElement| {
+            let pos = input.selection_start().unwrap_or(Some(0)).unwrap_or(0) as usize;
+            cursor_pos.set(pos);
+        })
     };
 
-    let on_input = {
+    let oninput = {
         let on_change = props.on_change.clone();
-        let update_selection = update_selection.clone();
+        let update_cursor = update_cursor.clone();
         Callback::from(move |e: InputEvent| {
             let input: HtmlInputElement = e.target_unchecked_into();
             on_change.emit(input.value());
-            update_selection(input);
+            update_cursor.emit(input);
         })
     };
 
-    let on_keydown = {
-        let update_selection = update_selection.clone();
+    let onkeyup = {
+        let update_cursor = update_cursor.clone();
         Callback::from(move |e: KeyboardEvent| {
             let input: HtmlInputElement = e.target_unchecked_into();
-            let update_selection = update_selection.clone();
-            spawn_local(async move {
-                sleep_ms(0).await;
-                update_selection(input);
-            });
+            update_cursor.emit(input);
         })
     };
 
-    let on_keyup = {
-        let update_selection = update_selection.clone();
-        Callback::from(move |e: KeyboardEvent| {
-            let input: HtmlInputElement = e.target_unchecked_into();
-            update_selection(input);
-        })
-    };
-
-    let on_click = {
-        let update_selection = update_selection.clone();
-        Callback::from(move |e: MouseEvent| {
-            let input: HtmlInputElement = e.target_unchecked_into();
-            update_selection(input);
-        })
-    };
-
-    let on_focus = {
-        let update_selection = update_selection.clone();
+    let onfocus = {
         let is_focused = is_focused.clone();
-        Callback::from(move |e: FocusEvent| {
-            is_focused.set(true);
-            let input: HtmlInputElement = e.target_unchecked_into();
-            let update_selection = update_selection.clone();
-            spawn_local(async move {
-                sleep_ms(0).await;
-
-                if let (Ok(Some(start)), Ok(Some(end))) =
-                    (input.selection_start(), input.selection_end())
-                {
-                    let value = input.value();
-                    let js_len = value.encode_utf16().count() as u32;
-
-                    if start == 0 && end == js_len && js_len > 0 {
-                        let _ = input.set_selection_range(js_len, js_len);
-                        update_selection(input);
-                    }
-                }
-            });
-        })
+        Callback::from(move |_| is_focused.set(true))
     };
 
-    let on_blur = {
+    let onblur = {
         let is_focused = is_focused.clone();
-        Callback::from(move |_: FocusEvent| {
-            is_focused.set(false);
-        })
+        Callback::from(move |_| is_focused.set(false))
     };
 
     let on_wrapper_click = {
@@ -123,71 +62,78 @@ pub fn shell_input(props: &ShellInputProps) -> Html {
         })
     };
 
-    html! {
-        <div class="shell-input-wrapper" onclick={on_wrapper_click}>
-            {
-                if props.value.is_empty() {
-                    html! {
-                        <>
-                            <span class="shell-input-content placeholder">{ &props.placeholder }</span>
-                            <span class="shell-cursor" style="position: absolute; left: 0;">{"_"}</span>
-                        </>
-                    }
-                } else {
-                    let chars: Vec<char> = props.value.chars().collect();
-                    let start = *selection_start;
-                    let end = *selection_end;
+    let render_content = {
+        let value = props.value.as_str();
+        let pos = *cursor_pos;
+        let is_focused = *is_focused;
 
-                    if start != end && *is_focused {
-                        // Selection mode
-                        let min = start.min(end);
-                        let max = start.max(end);
-                        let left: String = chars[0..min].iter().collect();
-                        let selected: String = chars[min..max].iter().collect();
-                        let right: String = chars[max..].iter().collect();
-                        html! {
-                            <span class="shell-input-content">
-                                { left }
-                                <span class="shell-input-selection">{ selected }</span>
-                                { right }
-                            </span>
-                        }
-                    } else {
-                        // Cursor mode
-                        let pos = start;
-                        if pos >= chars.len() {
-                            html! {
-                                <span class="shell-input-content">
-                                    { &props.value }
-                                    <span class="shell-cursor">{"_"}</span>
-                                </span>
-                            }
-                        } else {
-                            let left: String = chars[0..pos].iter().collect();
-                            let current = chars[pos];
-                            let right: String = chars[pos+1..].iter().collect();
-                            html! {
-                                <span class="shell-input-content">
-                                    { left }
-                                    <span class="shell-cursor-char">{ current }</span>
-                                    { right }
-                                </span>
-                            }
-                        }
+        if value.is_empty() {
+            let placeholder = props.placeholder.as_str();
+            if placeholder.is_empty() {
+                html! {
+                    if is_focused {
+                        <span class="shell-cursor-char">{" "}</span>
                     }
                 }
+            } else {
+                let mut chars = placeholder.chars();
+                let first = chars.next().unwrap_or(' ');
+                let rest: String = chars.collect();
+
+                html! {
+                    <span class="shell-input-content placeholder">
+                        if is_focused {
+                            <span class="shell-cursor-char">{ first }</span>
+                        } else {
+                            { first }
+                        }
+                        { rest }
+                    </span>
+                }
             }
+        } else {
+            let chars: Vec<char> = value.chars().collect();
+            let len = chars.len();
+
+            let pos = pos.min(len);
+
+            if !is_focused {
+                html! { <span class="shell-input-content">{ value }</span> }
+            } else if pos >= len {
+                html! {
+                    <span class="shell-input-content">
+                        { value }
+                        // FIXME
+                        <span class="shell-cursor-char" style="background-color: var(--bg-shell)">{" "}</span>
+                    </span>
+                }
+            } else {
+                let left: String = chars[0..pos].iter().collect();
+                let current = chars[pos];
+                let right: String = chars[pos + 1..].iter().collect();
+                html! {
+                    <span class="shell-input-content">
+                        { left }
+                        <span class="shell-cursor-char">{ current }</span>
+                        { right }
+                    </span>
+                }
+            }
+        }
+    };
+
+    html! {
+        <div class="shell-input-wrapper" onclick={on_wrapper_click}>
+            { render_content }
             <input
                 ref={input_ref}
                 class="shell-input-hidden"
                 type="text"
-                value={props.value.clone()}
-                oninput={on_input}
-                onkeydown={on_keydown}
-                onkeyup={on_keyup}
-                onclick={on_click}
-                onfocus={on_focus}
-                onblur={on_blur}
+                value={&props.value}
+                oninput={oninput}
+                onkeyup={onkeyup}
+                onfocus={onfocus}
+                onblur={onblur}
                 disabled={props.disabled}
             />
         </div>
